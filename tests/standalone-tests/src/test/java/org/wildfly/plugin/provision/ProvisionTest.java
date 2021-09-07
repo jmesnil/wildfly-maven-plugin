e7d59ba2884c780d0eb46ad1e040e826ccdb784a
JF Denise <jdenise@redhat.com>
2021-09-07 15:29:25 +0200

    Fix java package of new tests

--- tests/standalone-tests/src/test/java/org/wildfly/plugin/provision/ProvisionTest.java
+++ tests/standalone-tests/src/test/java/org/wildfly/plugin/provision/ProvisionTest.java
@@ -19,7 +19,7 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-package org.wildfly.plugin.cli.provision;
+package org.wildfly.plugin.provision;
 
 
 import org.apache.maven.plugin.Mojo;
